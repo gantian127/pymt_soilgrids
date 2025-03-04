43e53de0bbf385a3cf3955d192d4435228a593bb
gantian127 <jamy127@foxmail.com>
2025-03-04 15:53:39 -0700

    clean code

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(filename):
         return fp.read()
 
 
-long_description = u"\n\n".join(
+long_description = "\n\n".join(
     [read("README.rst"), read("CREDITS.rst"), read("CHANGES.rst")]
 )
 
@@ -24,7 +24,7 @@ setup(
     author_email="gantian127@gmail.com",
     description="PyMT plugin for pymt_soilgrids",
     long_description=long_description,
-    version="0.1.1",
+    version="0.1.3",
     url="https://github.com/gantian127/pymt_soilgrids",
     classifiers=[
         "Development Status :: 4 - Beta",
