ccf0d3c4186af381c376515433b24baae94c467e
gantian127 <jamy127@foxmail.com>
2025-03-04 15:26:34 -0700

    clean code

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 #! /usr/bin/env python
-import os
-import sys
-
-from setuptools import Extension, find_packages, setup
+from setuptools import find_packages, setup
 
 entry_points = {
     "pymt.plugins": [
